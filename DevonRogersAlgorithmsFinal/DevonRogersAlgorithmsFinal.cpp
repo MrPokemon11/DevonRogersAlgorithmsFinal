f82b907d4bf36b3f81c476b1047b885fe041599d
MrPokemon11 <devon@therogers.ca>
2024-03-21 15:00:19 -0400

    add new functions

--- DevonRogersAlgorithmsFinal/DevonRogersAlgorithmsFinal.cpp
+++ DevonRogersAlgorithmsFinal/DevonRogersAlgorithmsFinal.cpp
@@ -12,19 +12,26 @@ using namespace std;
 
 //create vectors with visited and unvisited nodes (follow Lecture 9)
 
+int convertCharToIndex(char in) {
+    int out;
+    out = in - 65;//65 is the character A, which will be equal to index 0
+    return out;
+}
+
 struct node {
     char id;
     //the character A is 65; make nodes using for loop
+    //the character W is 87; this will be the upper bound
     bool isChargingStation;
     int shortestDistFromSource = -1;//shortest distance from source; make sure that the source is equal to 0!
-        //costs for unvisited nodes are initially -1, aka infinity
+    //costs for unvisited nodes are initially -1, aka infinity
     node* prevNode; //previous node; set at the same time as shortestDistFromSource
 };
 
 /*
 Plan:
 - have network be linked list of nodes?
-    - 
+    -
 */
 
 struct network {
@@ -45,8 +52,8 @@ bool loadNetwork() {
 
     if (networkFile.ifstream::is_open()) {
         //do file loading
-
-        char nodeID;
+        cout << "File found!";
+        char nodeID = '0';
         //set nodeID to the current node ID pulled from the stream
         if (nodeID == 'H' || nodeID == 'K' || nodeID == 'Q' || nodeID == 'T') {
             //make it a charging station
@@ -57,14 +64,15 @@ bool loadNetwork() {
     }
     else {
         cout << "Network not found!\n";
-    }
 
+    }
+    return false;
 }
 
 //reminder to self: look at the pseudocode in lecture 10, should be very helpful
 
 //dijkstra's algorithm; i just don't want to type that every time
-void djAlgorithm() {
+void djAlgorithm(char start) {
     int currCost = 0;//the total weights of the edges to reach the current node 
     int bestCost = -1;
     bool isUnvisited = false;
@@ -81,11 +89,29 @@ void djAlgorithm() {
 
 int main()
 {
-    //get source node
+    //step 1: load the csv file
+    loadNetwork();
 
-    //if the source node *is* a charging station, you still need to find the distance to the other 3 '-_-
+    bool running = true;
+    while (running) {
 
-    std::cout << "Hello World!\n";
+        //step 2: get source node
+
+
+        //if the source node *is* a charging station, you still need to find the distance to the other 3 '-_-
+
+
+
+        //ask the user if they wish to find the charging stations from a different starting node
+        char ans;
+        cout << "Would you like to find the distance to the charging stations from another starting node? Y/N ";
+        cin >> ans;
+        if (tolower(ans) != 'y') {
+            running = false;
+        }
+    }
+
+    std::cout << "See you next time!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
