11178f9f117cbf00918fd89e8d5a67ffecd3d475
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2024-03-23 11:41:48 -0400

    "complete" network loading

--- DevonRogersAlgorithmsFinal/DevonRogersAlgorithmsFinal.cpp
+++ DevonRogersAlgorithmsFinal/DevonRogersAlgorithmsFinal.cpp
@@ -7,6 +7,8 @@
 #include <sstream>
 #include <iomanip>
 #include <fstream>
+#include <chrono>
+#include <map>
 
 using namespace std;
 
@@ -19,6 +21,14 @@ struct node {
     int shortestDistFromSource = -1;//shortest distance from source; make sure that the source is equal to 0!
         //costs for unvisited nodes are initially -1, aka infinity
     node* prevNode; //previous node; set at the same time as shortestDistFromSource
+};
+
+//the edges from one node to another and its weight
+struct adjNode {
+    char id;
+    int cost;
+
+    adjNode* link;
 };
 
 /*
@@ -36,9 +46,24 @@ struct network {
     network* link;
 };
 
+//adjacency list
+adjNode *adjacencyList[23];
 
+bool checkIsCharger(char nodeID) {
+    //set nodeID to the current node ID pulled from the stream
+    if (nodeID == 'H' || nodeID == 'K' || nodeID == 'Q' || nodeID == 'T') {
+        //make it a charging station
+        return true;
+    }
+    else {
+        //make it not a charging station
+        return false;
+    }
+}
 
-bool loadNetwork() {
+adjNode* adjFirst, * adjLast, * newAdj, *adjCurr;
+
+void loadNetwork() {
 
     string networkDat;
     ifstream networkFile("DevonRogersAlgorithmsFinalSpreadsheet.csv");
@@ -46,14 +71,65 @@ bool loadNetwork() {
     if (networkFile.ifstream::is_open()) {
         //do file loading
 
-        char nodeID;
-        //set nodeID to the current node ID pulled from the stream
-        if (nodeID == 'H' || nodeID == 'K' || nodeID == 'Q' || nodeID == 'T') {
-            //make it a charging station
+        int currentLine = 0;
+
+        while (getline(networkFile,networkDat)) {
+
+            //newAdj = new adjNode;
+
+            vector<string> tempArray;
+            vector<char> adjTemp;
+            vector<int> costTemp;
+
+            std::istringstream iss(networkDat);
+            string s;
+            while (std::getline(iss, s, ',')) {
+                tempArray.push_back(s);
+
+            }
+
+            std::istringstream adjNodes(tempArray[1]);
+            string adj;
+            while (std::getline(adjNodes, adj, ';')) {
+                adjTemp.push_back(adj[0]);
+            }
+
+            std::istringstream adjCosts(tempArray[2]);
+            string w;
+            while (std::getline(adjCosts, w, ';')) {
+                costTemp.push_back(stoi(w));
+            }
+
+
+            for (int i = 0; i < adjTemp.size();i++) {
+                newAdj = new adjNode;
+
+                newAdj->id = adjTemp[i];
+                newAdj->cost = costTemp[i];
+
+                newAdj->link = nullptr;
+
+                //from page 274/275 of the Malik C++ textbook
+                if (adjFirst == nullptr) {
+                    adjFirst = newAdj;
+                    adjLast = newAdj;
+                }
+                else {
+                    adjLast->link = newAdj;
+                    adjLast = newAdj;
+                }
+            }
+
+            adjacencyList[currentLine] = adjFirst;
+
+            //keep this at the end of the while loop
+            adjFirst = nullptr;
+            adjLast = nullptr;
+
+            currentLine++;
         }
-        else {
-            //make it not a charging station
-        }
+
+
     }
     else {
         cout << "Network not found!\n";
@@ -79,8 +155,32 @@ void djAlgorithm() {
     }
 }
 
+//prints all data from the given line
+void printData() {
+
+    for (int i = 0; i < 23;i++) {
+
+        adjCurr = adjacencyList[i];
+
+        while (adjCurr != NULL) {
+            cout << adjCurr->id << ", " << adjCurr->cost << endl;
+            adjCurr = adjCurr->link;
+        }
+        cout << endl;
+    }
+
+
+}
+
 int main()
 {
+    //load network
+    loadNetwork();
+
+    //debug: print the network
+    printData();
+
+
     //get source node
 
     //if the source node *is* a charging station, you still need to find the distance to the other 3 '-_-
